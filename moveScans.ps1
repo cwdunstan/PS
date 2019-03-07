09384b5ec50ae6ef9d99f4309ae45d40bf3ba9c5
Charlie Dunstan <35953283+cwdunstan@users.noreply.github.com>
2019-03-07 11:14:12 +1100

    MoveScans3.0
    Script written to move incoming scans (JPEG & PDF) to another file directory, renaming them to avoid over-writing.
    Note: resets nightly as to reset file count.

---
+++ moveScans.ps1
@@ -0,0 +1,33 @@
+﻿#MoveScan3.0 Made by Charlie and Simon for the Techroom Server.
+
+$jpgcount=0
+$pdfcount=0
+write-host("This script is monitoring the scans to the server. If found they are transferred from TBU server to Tech Server. Please leave me alone.")
+while(1){
+
+$timeday= Get-Date -UFormat %R
+
+    $files = Get-ChildItem z:\* -Include *.jpg, *.pdf | where Length -gt 1kb
+
+    foreach ($file in $files) {
+        if ($file -like "*.jpg" ){
+            $jpgcount++
+            $files = Get-ChildItem C:\Users\chdunsta\Desktop\test\* -Include *.jpg | Move-Item -Destination "D:\Scans\SCAN $jpgcount $(Get-Date -f hh-mm-ss).jpg" -ErrorAction SilentlyContinue
+
+        }
+        if ($file -like "*.pdf" ){
+            $pdfcount++
+            $files = Get-ChildItem C:\Users\chdunsta\Desktop\test\* -Include *.pdf | Move-Item -Destination "D:\Scans\PDF $pdfcount $(Get-Date -f hh-mm-ss).pdf" -ErrorAction SilentlyContinue
+        }
+
+        start-sleep -s 1
+}
+
+
+    if($timeday -eq "23:59"){
+        $jpgcount=0
+        $pdfcount=0
+    }
+
+    Start-Sleep -s 3
+}
